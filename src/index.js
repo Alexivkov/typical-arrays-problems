495706a2e012fb7601478e9836b82510dc720377
alexivkov <sivkov@inbox.ru>
2021-08-08 12:08:38 +0300

    feat:add min max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if (array === undefined || !array.length) return 0;
+    let k = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < k) {
+            k = array[i];
+        }
+    }
+    return k;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || !array.length) return 0;
+    let k = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > k) {
+            k = array[i];
+        }
+    }
+    return k;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || !array.length) return 0;
+    let k = array[0];
+    for (let i = 1; i < array.length; i++) {
+        k += array[i];
+    }
+    return k / array.length;
+};
